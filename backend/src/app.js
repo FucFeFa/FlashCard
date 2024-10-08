0f0a081a87c6851a0df4f609ea09e83e60a60c96
Phuc Pham <phamphuc01072003@gmail.com>
2024-10-08 11:37:02 +0700

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -23,6 +23,8 @@ app.use(session({
         secure: false,
         httpOnly: true, // Ngan chan truy cap JavaScript phia client
         maxAge: 1000 * 60 * 60 * 24, // Cookie ton tai trong 1 ngay
+        domain: 'localhost', // Dam bao cookie co the duoc chia se qua cac cong khac nhau tren localhost
+        sameSite: 'Lax' // Đảm bảo cookie được chia sẻ giữa các trang khác nhau
     }
 }))
 
